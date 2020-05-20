5a6dee68d2095a78fc095bcd1839e5f3b9f18890
Kira <datdhvinh@gmail.com>
2020-05-20 16:36:41 +0700

    Copy file text

--- src/CopyFileCharacter.java
+++ src/CopyFileCharacter.java
@@ -3,30 +3,35 @@ import java.io.*;
 public class CopyFileCharacter {
     public static void main(String[] args) {
         try {
-            //Tao file
+            //Tao và thêm dữ liệu cho file
 
-            BufferedWriter bufferedWriter=new BufferedWriter(new  FileWriter("input.txt"));
-            bufferedWriter.write("Hello world");
-            bufferedWriter.close();
+//            BufferedWriter bufferedWriter=new BufferedWriter(new  FileWriter("input.txt"));
+//            bufferedWriter.write("Hello world");
+//            bufferedWriter.close();
+
+            //check file tồn tại hay ko
+            File file=new File("input.txt");
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
 
             //Đọc và copy file
-            FileReader fileReader = new FileReader("input.txt");
-            FileWriter fileWriter = new FileWriter("output.txt");
-//            File file=new File(String.valueOf(fileReader));
-//            if (file.exists()) {
-//                throw new Exist
-//            }
+//            FileReader fileReader = new FileReader("input.txt");
+            BufferedReader bufferedReader=new BufferedReader(new FileReader(file));
+//            FileWriter fileWriter = new FileWriter("output.txt");
+            BufferedWriter fileWriter=new BufferedWriter(new FileWriter("output.txt"));
+
             int copy;
             int count=0;
-            while ((copy = fileReader.read()) != -1) {
+            while ((copy = bufferedReader.read()) != -1) {
                 fileWriter.write(copy);
                 count++;
             }
             System.out.println("Sô ký tự trong file: "+count);
-            fileReader.close();
+            bufferedReader.close();
             fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("File không tồn tại");
         }
     }
 }
